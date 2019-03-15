1d871f88d0fbc24dd3c0f61a3609e760bd01bb5c
nunof <nunofag9@hotmail.com>
2019-03-15 15:06:45 +0000

    ex02 feito

---
+++ Ficha03/ex02js.js
@@ -0,0 +1,8 @@
+//ALINEA B
+document.getElementById("p1").innerHTML="Está um dia de chuva";
+
+//ALINEA C
+document.querySelector("img").src= "https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg";
+
+//ALINEA D
+document.getElementById("p2").style.backgroundColor = "yellow";
